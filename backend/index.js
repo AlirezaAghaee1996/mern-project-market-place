ba10ccc57c3c90b3a39c5abac71b0e8534c1d991
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2023-12-18 22:49:46 +0330

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
-import express from 'express'
-const app=express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+mongoose
+  .connect(process.env.DATABASE_ADDRESS)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => console.log(err));
 
+const app = express();
 
-
-
-
-app.listen(3000,()=>{
-    console.log('server start')
-})
+app.listen(3000, () => {
+  console.log("server start");
+});
