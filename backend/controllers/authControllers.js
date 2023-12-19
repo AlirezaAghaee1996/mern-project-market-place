ce48a27baa00d8dd3bdc3b6800f1473a91ffa875
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2023-12-19 22:39:38 +0330

    add signup controllers

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,12 +1,18 @@
 import User from "../models/userModel.js"
-
+import bcryptjs from 'bcryptjs'
 export const signUp=async(req,res)=>{
     const {username,password,email}=req.body
-    const newUser=new User({username,password,email})
-    await newUser.save()
-    res.status(201).json({
-        status:'success',
-        message:'signup successfully'
-    })
+    const hashedPassword=bcryptjs.hashSync(password,10)
+    const newUser=new User({username,password:hashedPassword,email})
+    try{
+        await newUser.save()
+        res.status(201).json({
+            status:'success',
+            message:'signup successfully'
+        })
+    }catch(err){
+        res.status(500).json(err.message)
+    }
+    
 
 }
