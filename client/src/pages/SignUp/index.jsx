db5b91114dd15805fb415cfc80d9c0304bbb3ebf
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2023-12-25 20:42:00 +0330

    add use form fields
    add use form fields for handle form

--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,12 +1,13 @@
 
 export default function SignUp() {
   return (
-    <div>
+    <div className="p-3 mx-auto max-w-lg">
       <h1 className="text-3xl text-center my-7 font-semibold">Sign Up</h1>
-      <form>
+      <form className="flex flex-col gap-4">
       <input type="text" className="border p-3 rounded-lg" placeholder="Username"/>
       <input type="email" className="border p-3 rounded-lg" placeholder="Email"/>
       <input type="password" className="border p-3 rounded-lg" placeholder="Password"/>
+      <button className="bg-sky-900 text-white">Sign Up</button>
       </form>
     </div>
   )
