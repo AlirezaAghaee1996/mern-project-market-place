5e6ab9df6549baaba415be4394be96b26097659c
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2024-01-09 22:02:24 +0330

    create Login Api

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { signUp,signIn } from '../controllers/authControllers.js'
 const router=express.Router()
 router.post('/signup',signUp)
-router.post('/',signIn)
+router.post('/signin',signIn)
 
 
 
